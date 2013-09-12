5d7e031b74ecd2dc194ae02b8a84cdadd035c980
RIVIERE Gregory <gregory.riviere@laposte.net>
2013-09-12 14:07:50 +0200

    Test envoie xml

--- src/fr/treeptik/samplejms/Producteur.java
+++ src/fr/treeptik/samplejms/Producteur.java
@@ -1,12 +1,19 @@
 package fr.treeptik.samplejms;
 
-import javax.jms.ObjectMessage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 import javax.jms.Queue;
 import javax.jms.QueueConnection;
 import javax.jms.QueueConnectionFactory;
 import javax.jms.QueueSender;
 import javax.jms.QueueSession;
+import javax.jms.TextMessage;
 import javax.naming.InitialContext;
+
+import fr.treeptik.samplejms.xml.Client;
+import fr.treeptik.samplejms.xml.Personne;
 
 public class Producteur {
 
@@ -28,10 +35,46 @@ public class Producteur {
 		// TextMessage message = session.createTextMessage("Hello JMS");
 		// sender.send(message);
 
-		Produit produit = new Produit(1, "Produit 1", "Super Produit 1");
-
+		// Produit produit = new Produit(1, "Produit 1", "Super Produit 1");
 		// Envoie de message Objet serialisable
-		ObjectMessage message = session.createObjectMessage(produit);
+		// ObjectMessage message = session.createObjectMessage(produit);
+		// sender.send(message);
+
+		// Pour l'envoie de fichier xml
+		// JAXBContext jaxbContext = JAXBContext.newInstance("fr.treeptik.samplejms.xml");
+
+		Client client = new Client();
+		Personne personne = new Personne();
+		personne.setNom("River");
+		personne.setPrenom("Greg");
+		client.getPersonne().add(personne);
+		personne = new Personne();
+		personne.setNom("Paul");
+		personne.setPrenom("ET");
+		client.getPersonne().add(personne);
+		personne = new Personne();
+		personne.setNom("Sky");
+		personne.setPrenom("Luck");
+		client.getPersonne().add(personne);
+		// Création du fichier client
+		// Marshaller marshaller = jaxbContext.createMarshaller();
+		// marshaller.marshal(client, new File("client.xml"));
+
+		// Lecture du fichier client
+		// Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+		// Client newClient = (Client) unmarshaller.unmarshal(new File("client.xml"));
+		// System.out.println(newClient.getPersonne().size());
+
+		File fileEcriture = new File("client.xml");
+		StringBuffer line = new StringBuffer();
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(fileEcriture));
+		while (bufferedReader.ready()) {
+			line.append(bufferedReader.readLine());
+		}
+		System.out.println(line.toString());
+		bufferedReader.close();
+
+		TextMessage message = session.createTextMessage(line.toString());
 		sender.send(message);
 
 		session.close();
@@ -39,5 +82,4 @@ public class Producteur {
 		System.out.println("Message envoyé");
 
 	}
-
 }
